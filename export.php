072bcb1bf22b059a2b7b0ceda2cd2f7688124c67
Mubalama Richard <mubalama@live.com>
2019-10-04 18:16:07 +1000

    New Comment added

--- export.php
+++ export.php
@@ -3,6 +3,7 @@
  if(isset($_POST["export"]))  
  {  
       $connect = mysqli_connect("localhost", "root", "", "csv");  
+      // This the comment to test the git command
       header('Content-Type: text/csv; charset=utf-8');  
       header('Content-Disposition: attachment; filename=data.csv');  
       $output = fopen("php://output", "w");  
